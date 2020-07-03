fb2e125d9d347f383b1ec387c45216f1fabdef78
冯淼森 <fengmiaosen@gmail.com>
2020-07-03 11:56:18 +0800

    promise.retry &forEach内调用await

---
+++ js基础/41. forEach循环输出考察/index.js
@@ -0,0 +1,42 @@
+// 输出以下代码运行结果，为什么？如果希望每隔 1s 输出一个结果，应该如何改造？注意不可改动 square 方法
+
+const list = [1, 2, 3]
+const square = num => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(num * num)
+        }, 1000)
+    })
+}
+
+// // 原题目
+// //forEach是不能阻塞的，默认是请求并行发起，所以是同时输出1、4、9。
+// 原理参照 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// function test() {
+//   list.forEach(async x=> {
+//     const res = await square(x)
+//     console.log(res)
+//   })
+// }
+
+// // 解决方案一：
+// // 改用for循环
+// async function test() {
+
+//     for (let i = 0; i < list.length; i++) {
+//         let res = await square(list[i]);
+//         console.log('res:', res);
+//     }
+// }
+
+// 解决方案二：
+// 改用for of循环
+async function test() {
+
+    for (let item of list) {
+        let res = await square(item);
+        console.log('res:', res);
+    }
+}
+
+test()
---
+++ js基础/41. forEach循环输出考察/readme.md
@@ -0,0 +1,43 @@
+普通的for 等于同一个块作用域连续await，而forEach的回调是一个个单独的函数，跟其他回调同时执行，互不干扰
+
+```js
+function test() {
+    list.forEach(async x=> {
+      const res = await square(x)
+      console.log(res)
+    })
+
+    //forEach循环等于三个匿名函数;
+    (async (x) => {
+        const res = await square(x)
+        console.log(res)
+    })(1);
+    (async (x) => {
+        const res = await square(x)
+        console.log(res)
+    })(2);
+    (async (x) => {
+        const res = await square(x)
+        console.log(res)
+    })(3);
+
+    // 上面的任务是同时进行
+  }
+
+  async function test() {
+    for (let x of list) {
+      const res = await square(x)
+      console.log(res)
+    }
+  }
+  //等价于
+
+  async function test() {
+      const res = await square(1)
+      console.log(res)
+      const res2 = await square(2)
+      console.log(res)
+      const res3 = await square(3)
+      console.log(res)
+  }
+```
