962c0b5497322e26037993975e15ceeb3127fd65
冯淼森 <fengmiaosen@gmail.com>
2020-07-22 20:06:12 +0800

    大数组汇中获取随机数

---
+++ 基础编程题/24.从巨大的数组中随机获取部分元素/3.js
@@ -0,0 +1,18 @@
+
+
+function randomSort(nums, len) {
+    const MAX_COUNT = nums.length;
+    const result = new Set();
+
+    while (result.size < len) {
+        const idx = Math.floor(MAX_COUNT * Math.random());
+        result.add(nums[idx]);
+    }
+
+    return Array.from(result);
+}
+
+
+let list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 130, 4567, 98, 76, 467, 387];
+
+console.log('random sort:', randomSort(list, 4))
